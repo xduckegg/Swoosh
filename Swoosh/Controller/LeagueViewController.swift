827f351c79e4610e8bb0a30f582f6f6b9e45fc66
Khaled Aldousari <ineedumore08@gmail.com>
2018-12-02 21:42:46 -0800

    Created a model

--- Swoosh/Controller/LeagueViewController.swift
+++ Swoosh/Controller/LeagueViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class LeagueViewController: UIViewController {
-
+    @IBOutlet weak var nextButton: BorderButton!
+    var player:Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player()
+    }
+    @IBAction func onMensTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "mens")
+        
+    }
+    @IBAction func onWomensTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "womens")
+        
+    }
+    @IBAction func onCoedTapped(_ sender: BorderButton) {
+        selectLeague(leagueType: "coed")
     }
     
-
+    func selectLeague(leagueType:String){
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
     
     // MARK: - Navigation
-
+    
     @IBAction func onNextTapped(_ sender: UIButton) {
         //goToImA
         
@@ -27,11 +43,11 @@ class LeagueViewController: UIViewController {
         
     }
     /*
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Khaled Aldousari on 12/2/18.
+//  Copyright © 2018 Khaled Aldousari. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague:String?
+    var selectedSkill:String?
+    
+}
